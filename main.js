edce12cba557ab7c746fdad73e2bfdfdc801721b
JoseRicardoR <josericardorinconpelaez@gmail.com>
2025-11-26 19:08:18 +0100

    star plot update

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 //References
 //https://observablehq.com/@observablehq/introduction-to-d3-course-lesson-2?collection=@observablehq/introduction-to-d3-course
+//https://observablehq.com/@observablehq/introduction-to-d3-course?collection=@observablehq/introduction-to-d3-course
 //https://d3-graph-gallery.com/graph/scatter_tooltip.html
 //https://d3-graph-gallery.com/graph/custom_theme.html
 //https://d3js.org/d3-shape/symbol#symbolDiamond
 //https://d3-graph-gallery.com/graph/custom_legend.html
 //https://d3js.org/d3-selection/joining
-
+//https://stackoverflow.com/questions/67463864/javascript-d3-plotting-radar-graph
+//https://observablehq.com/@d3/splom/2?collection=@d3/d3-scale
 
 
 // Waiting until document has loaded
@@ -20,13 +22,12 @@ window.onload = () => {
   var svg = d3.select("#chart")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .attr("border", "1px dotted #000");
+    .attr("height", height + margin.top + margin.bottom);
 
 
   d3.csv("cars.csv").then(data => {
     // Convert strings to numbers
-    data = data.filter(d => d.x !== null && d.y !== null);
+    data = data.filter(d => d["Dealer Cost"] !== null && d["Horsepower(HP)"] !== null);
 
     console.log("Loaded CSV data:", data);
 
@@ -97,7 +98,7 @@ window.onload = () => {
       .attr("transform", function (d, i) { return `translate(${width - 3},${(i * 15 + margin.top)})` })
       .style("fill", function (d) { return colorScale(d) })
       .style("opacity", 0.8)
-    
+
 
     // Add one dot in the legend for each name.
     svg.selectAll("legendlabels")
@@ -111,12 +112,14 @@ window.onload = () => {
       .attr("text-anchor", "left")
       .style("alignment-baseline", "middle")
 
+    var selected_data_index = 0;
+
     updateScatter("Retail Price", "Retail Price")
 
 
     function updateScatter(xCol, yCol) {
 
-      let filtered = data.map(d => ({
+      var filtered = data.map(d => ({
         name: d["Name"],
         x: +d[xCol],
         y: +d[yCol],
@@ -200,8 +203,19 @@ window.onload = () => {
             .duration(200)
             .style("opacity", 0)
         })
+        .on("click", function (f) {
+          //selected_data = data.filter(d => d["Name"] === f.name); 
+          //selected_data_index = data.filter((d) => d["Name"] === f.name); 
+
+          selected_data_index = data.map((d, i) => d["Name"] === f.name ? i : -1).filter(i => i !== -1)[0];
+
+          console.log(f.name);
+          console.log(selected_data_index);
+          updateStarPlot(selected_data_index);
+        })
 
     }
+
 
     // When the button is changed, run the updateChart function
     d3.selectAll(".selectList").on("change", function (d) {
@@ -211,6 +225,116 @@ window.onload = () => {
       // run the updateChart function with this selected option
       updateScatter(selectedOptionX, selectedOptionY)
     })
+
+
+    //-----------------------------------------------------------------------------
+
+
+    const radius = width * 0.25;
+    const center = { x: (width + margin.left + margin.right) * 0.5 + 15, y: (height + margin.top + margin.bottom) * 0.35 };
+
+    var svg_radar = d3.select("#radar_chart")
+      .append("svg")
+      .attr("width", (width + margin.left + margin.right))
+      .attr("height", (height + margin.top + margin.bottom) * 0.7);
+
+    const radialScale = d3.scaleLinear()
+      .domain([0, radius])
+      .range([radius, 0])
+
+    let val;
+    for (val = 0; val <= radius; val += radius / 5) {
+      const r = radialScale(val);
+      svg_radar.append('circle')
+        .attr('cx', center.x)
+        .attr('cy', center.y)
+        .attr('r', r)
+        .style('stroke', '#aaa')
+        .style('fill', 'none');
+    }
+
+    for (let index = 0; index < headers.length; index++) {
+      const angle = index * Math.PI * 2 / headers.length;
+      const x = center.x + radius * Math.sin(angle);
+      const y = center.y + radius * -Math.cos(angle);
+      //despues de la mitad del circulo, pi
+      const radial_anchor_rotation = angle > 3.15 ? "end" : "start";
+      const y_shift = angle > 3.15 ? 10 : 0;
+
+      svg_radar.append('line')
+        .attr('x1', center.x)
+        .attr('y1', center.y)
+        .attr('x2', x)
+        .attr('y2', y)
+        .style('stroke', '#000');
+
+      svg_radar.append('text')
+        .text(headers[index])
+        .attr('text-anchor', radial_anchor_rotation)
+        .attr('x', x)
+        .attr('y', y)
+    }
+
+    //First renderization
+
+    updateStarPlot(10);
+
+
+    function updateStarPlot(selected_data_index) {
+
+      console.log("Data from csv", data[selected_data_index]);
+
+      var data_radi = {
+        values: []
+      };
+
+      for (let key in data[selected_data_index]) {
+        if (key === "Name") {
+          data_radi.name = data[selected_data_index][key];
+        }
+        else if (key === "Type") {
+          data_radi.type = data[selected_data_index][key];
+        }
+        else {
+          var maxValue = d3.max(data, d => +d[key]);
+          var minValue = 0;
+          var value_normalized = (data[selected_data_index][key] - minValue) / (maxValue - minValue);
+          //console.log(key, maxValue,minValue,data[0][key], value_normalized);
+          data_radi.values.push(radius * value_normalized);
+        }
+      }
+      data_radi.color = colorScale(data_radi.type);
+      //console.log("Star data from the csv normalized",data_radi);
+
+      path_radial = '';
+      for (let i = 0; i < data_radi.values.length; i++) {
+        const r = radius - radialScale(data_radi.values[i]);
+        const angle = i * Math.PI * 2 / data_radi.values.length;
+        const x = center.x + r * Math.sin(angle);
+        const y = center.y + r * -Math.cos(angle);
+        path_radial += `${i > 0 ? 'L' : 'M'} ${x},${y} `;
+      }
+      path_radial += 'Z';
+
+      // Select the path (if it exists) or append it
+      let path = svg_radar.selectAll(".path")   // select existing path(s)
+        .data([data_radi]);                   // bind data as array (single element)
+
+      path.enter()
+        .append("path")
+        .attr("class", "path")
+        .merge(path)
+        .transition()
+        .duration(200)
+        .attr("class", "path")
+        .attr('d', path_radial)
+        .style('stroke', data_radi.color)
+        .style('stroke-width', 3)
+        .style('stroke-opacity', 0.6)
+        .style('fill', data_radi.color)
+        .style('fill-opacity', 0.3)
+
+    };
 
   }).catch(error => {
     console.error("Error loading CSV:", error);
